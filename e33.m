9ab0363da66d2ee284475694a6ee28f055a01a86
Hilding Wollbo <Hilding@n182-p119.eduroam.kth.se>
2019-01-25 17:59:26 +0100

    e33

---
+++ e33.m
@@ -0,0 +1,4 @@
+%% Exercise 3.3
+
+e31
+spec_add(p1,sigma2,p2,sigma2noise)
--- spec_add.m
+++ spec_add.m
@@ -1,1 +1,6 @@
 function [PhixyNum, PhixyDen, PhiyyNum, PhiyyDen] = spec_add(A, sigma2, Anoise, sigma2noise)
+
+[PhixyNum, PhixyDen] = filtspec(1, A, sigma2)
+[PhiyyNum, PhiyyDen] = filtspec(1, Anoise, sigma2noise)
+
+end
